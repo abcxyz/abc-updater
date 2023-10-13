83a677a4a505b30c8d6176a9ace0928faba1b5c4
Kevin Lee <kkevinlee@google.com>
2023-10-13 15:17:04 -0700

    Init terraform directory with GCS (#2)
    gcs and related iam
    intended to be public bucket, will be backend for gclb

---
+++ terraform/gcs.tf
@@ -0,0 +1,37 @@
+# Copyright 2023 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_storage_bucket" "default" {
+  project = var.project_id
+
+  name          = var.bucket_name
+  location      = "US"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "object_admins" {
+  for_each = toset(var.bucket_object_admins)
+
+  bucket = google_storage_bucket.default.name
+  role   = "roles/storage.objectAdmin"
+  member = each.key
+}
+
+resource "google_storage_bucket_iam_member" "object_viewers" {
+  bucket = google_storage_bucket.default.name
+  role = "roles/storage.objectViewer"
+  member = "allUsers"
+}
