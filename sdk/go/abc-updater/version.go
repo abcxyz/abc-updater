8e774d3bf1367cc3c463d93476f4b9bc2bfd150d
Kevin Lee <kkevinlee@google.com>
2023-10-25 17:00:16 -0700

    Add simple method for checking app verison (#7)
    Will query server for latest version info and output version update
    message if running version is out of date.
    
    Future PRs will handle opt out and how frequently the check is
    performed.

--- sdk/go/abc-updater/version.go
+++ sdk/go/abc-updater/version.go
@@ -14,9 +14,107 @@
 
 package abcupdater
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
 
-// CheckAppVersion placeholder function.
-func CheckAppVersion() {
-	fmt.Printf("TODO Version Check")
+	"github.com/sethvargo/go-envconfig"
+	"golang.org/x/mod/semver"
+)
+
+// CheckVersionParams are the parameters for checking for application updates.
+type CheckVersionParams struct {
+	// The ID of the application to check.
+	AppID string
+
+	// The version of the app to check for updates.
+	// Should be of form vMAJOR[.MINOR[.PATCH[-PRERELEASE][+BUILD]]] (e.g., v1.0.1)
+	Version string
+
+	// The writer where the update info will be written to.
+	Writer io.Writer
+
+	// An optional configLookuper to supply config values. Will default to os environment variables.
+	ConfigLookuper envconfig.Lookuper
+}
+
+// AppResponse is the response object for an app version request.
+// It contains information about the most recent version for a given app.
+type AppResponse struct {
+	AppID          string `json:"app_id"`
+	AppName        string `json:"app_name"`
+	GitHubURL      string `json:"github_url"`
+	CurrentVersion string `json:"current_version"`
+}
+
+type config struct {
+	ServerURL      string        `env:"ABC_UPDATER_URL,default=https://abc-updater-autopush.tycho.joonix.net"`
+	RequestTimeout time.Duration `env:"ABC_UPDATER_TIMEOUT,default=2m"`
+}
+
+const (
+	appDataURLFormat      = "%s/%s/data.json"
+	outputFormat          = "A new version of %s is available! Your current version is %s. Version %s is available at %s.\n"
+	maxErrorResponseBytes = 2048
+)
+
+// CheckAppVersion checks if a newer version of an app is available. Relevant update info will be
+// written to the writer provided if applicable.
+func CheckAppVersion(ctx context.Context, params *CheckVersionParams) {
+	lookuper := params.ConfigLookuper
+	if lookuper == nil {
+		lookuper = envconfig.OsLookuper()
+	}
+
+	var c config
+	if err := envconfig.ProcessWith(ctx, &c, lookuper); err != nil {
+		return
+	}
+
+	// Use ParseRequestURI over Parse because Parse validation is more loose and will accept
+	// things such as relative paths without a host.
+	if _, err := url.ParseRequestURI(c.ServerURL); err != nil {
+		return
+	}
+
+	if !semver.IsValid(params.Version) {
+		return
+	}
+
+	client := &http.Client{
+		Timeout: c.RequestTimeout,
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf(appDataURLFormat, c.ServerURL, params.AppID), nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+
+	var result AppResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return
+	}
+
+	// semver requires v prefix. Current version data is stored without prefix so prepend v.
+	if semver.Compare(params.Version, "v"+result.CurrentVersion) < 0 {
+		outStr := fmt.Sprintf(outputFormat, result.AppName, params.Version, result.CurrentVersion, result.GitHubURL)
+		if _, err := params.Writer.Write([]byte(outStr)); err != nil {
+			return
+		}
+	}
 }
--- sdk/go/abc-updater/version_test.go
+++ sdk/go/abc-updater/version_test.go
@@ -14,9 +14,106 @@
 
 package abcupdater
 
-import "testing"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/sethvargo/go-envconfig"
+)
 
 func TestCheckAppVersion(t *testing.T) {
 	t.Parallel()
-	t.Skipf("TODO")
+
+	sampleAppResponse, err := json.Marshal(AppResponse{
+		AppID:          "sample_app_1",
+		AppName:        "Sample App 1",
+		GitHubURL:      "https://github.com/abcxyz/sample_app_1",
+		CurrentVersion: "1.0.0",
+	})
+	if err != nil {
+		t.Errorf("failed to encode json %v", err)
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasSuffix(r.RequestURI, "sample_app_1/data.json") {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintln(w, http.StatusText(http.StatusNotFound))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, string(sampleAppResponse))
+	}))
+
+	lookuper := envconfig.MapLookuper(map[string]string{
+		"ABC_UPDATER_URL": ts.URL,
+	})
+
+	t.Cleanup(func() {
+		ts.Close()
+	})
+
+	cases := []struct {
+		name    string
+		appID   string
+		version string
+		want    string
+	}{
+		{
+			name:    "outdated_version",
+			appID:   "sample_app_1",
+			version: "v0.0.1",
+			want: fmt.Sprintf(outputFormat,
+				"Sample App 1",
+				"v0.0.1",
+				"1.0.0",
+				"https://github.com/abcxyz/sample_app_1"),
+		},
+		{
+			name:    "current_version",
+			appID:   "sample_app_1",
+			version: "v1.0.0",
+			want:    "",
+		},
+		{
+			name:    "invalid_app_id",
+			appID:   "bad_app",
+			version: "v1.0.0",
+			want:    "",
+		},
+		{
+			name:    "invalid_version",
+			appID:   "sample_app_1",
+			version: "1.0.0.12.2",
+			want:    "",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			var b bytes.Buffer
+			params := &CheckVersionParams{
+				AppID:          tc.appID,
+				Version:        tc.version,
+				Writer:         &b,
+				ConfigLookuper: lookuper,
+			}
+
+			CheckAppVersion(context.Background(), params)
+
+			if got, want := b.String(), tc.want; got != want {
+				t.Errorf("incorrect output got=%s, want=%s", got, want)
+			}
+		})
+	}
 }
