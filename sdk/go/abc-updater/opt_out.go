411a06f0be1e61efe2098b261ed4b9102d5ccb56
Kevin Lee <kkevinlee@google.com>
2024-01-05 10:10:36 -0800

    create opt out settings via environment variables (#8)
    add ability to parse opt out settings from environment variables
    
    two environment variables added:
    one for opting out of specific version updates
    one for opting out of all version updates
    
    future PR will use these settings to determine version check behavior

---
+++ sdk/go/abc-updater/opt_out.go
@@ -0,0 +1,50 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package abcupdater
+
+import (
+	"context"
+	"strings"
+
+	"github.com/sethvargo/go-envconfig"
+)
+
+type optOutSettings struct {
+	IgnoreVersions    []string `env:"IGNORE_VERSIONS"`
+	ignoreAllVersions bool
+	errorLoading      bool
+}
+
+// loadOptOutSettings will return an optOutSettings struct populated based on the lookuper provided.
+func loadOptOutSettings(ctx context.Context, lookuper envconfig.Lookuper, appID string) *optOutSettings {
+	l := envconfig.PrefixLookuper(envVarPrefix(appID), lookuper)
+	var c optOutSettings
+	if err := envconfig.ProcessWith(ctx, &c, l); err != nil {
+		c.errorLoading = true
+		return &c
+	}
+
+	for _, version := range c.IgnoreVersions {
+		if strings.ToLower(version) == "all" {
+			c.ignoreAllVersions = true
+		}
+	}
+
+	return &c
+}
+
+func envVarPrefix(appID string) string {
+	return strings.ToUpper(appID) + "_"
+}
---
+++ sdk/go/abc-updater/opt_out_test.go
@@ -0,0 +1,115 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package abcupdater
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/sethvargo/go-envconfig"
+)
+
+func TestLoadOptOutSettings(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name        string
+		appID       string
+		lookuperMap map[string]string
+		want        *optOutSettings
+	}{
+		{
+			name:        "no_env_vars_set",
+			appID:       "sample_app_1",
+			lookuperMap: map[string]string{},
+			want: &optOutSettings{
+				ignoreAllVersions: false,
+				IgnoreVersions:    nil,
+				errorLoading:      false,
+			},
+		},
+		{
+			name:  "set_ignore_all",
+			appID: "sample_app_1",
+			lookuperMap: map[string]string{
+				"SAMPLE_APP_1_IGNORE_VERSIONS": "all",
+			},
+			want: &optOutSettings{
+				ignoreAllVersions: true,
+				IgnoreVersions:    []string{"all"},
+				errorLoading:      false,
+			},
+		},
+		{
+			name:  "set_ignore_single_version",
+			appID: "sample_app_1",
+			lookuperMap: map[string]string{
+				"SAMPLE_APP_1_IGNORE_VERSIONS": "1.0.0",
+			},
+			want: &optOutSettings{
+				ignoreAllVersions: false,
+				IgnoreVersions:    []string{"1.0.0"},
+				errorLoading:      false,
+			},
+		},
+		{
+			name:  "set_ignore_multiple_version",
+			appID: "sample_app_1",
+			lookuperMap: map[string]string{
+				"SAMPLE_APP_1_IGNORE_VERSIONS": "<1.0.0,2.0.0,3.0.0",
+			},
+			want: &optOutSettings{
+				ignoreAllVersions: false,
+				IgnoreVersions:    []string{"<1.0.0", "2.0.0", "3.0.0"},
+				errorLoading:      false,
+			},
+		},
+		{
+			name:  "invalid_app_id",
+			appID: "sample app 1",
+			lookuperMap: map[string]string{
+				"SAMPLE_APP_1_IGNORE_VERSIONS": "<1.0.0,2.0.0,3.0.0",
+			},
+			want: &optOutSettings{
+				ignoreAllVersions: false,
+				IgnoreVersions:    nil,
+				errorLoading:      false,
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			config := loadOptOutSettings(context.Background(), envconfig.MapLookuper(tc.lookuperMap), tc.appID)
+
+			if diff := cmp.Diff(tc.want.IgnoreVersions, config.IgnoreVersions); diff != "" {
+				t.Errorf("Config unexpected diff (-want,+got):\n%s", diff)
+			}
+
+			if got, want := config.ignoreAllVersions, tc.want.ignoreAllVersions; got != want {
+				t.Errorf("incorrect ignoreAllVersions got=%t, want=%t", got, want)
+			}
+
+			if got, want := config.errorLoading, tc.want.errorLoading; got != want {
+				t.Errorf("incorrect errorLoading got=%t, want=%t", got, want)
+			}
+		})
+	}
+}
