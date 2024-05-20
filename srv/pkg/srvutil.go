2a7361baa2290b5279564c43b290d702d3036326
pdewilde <pdewilde@google.com>
2024-05-20 14:17:33 -0700

    fix return types

--- srv/pkg/srvutil.go
+++ srv/pkg/srvutil.go
@@ -19,9 +19,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/abcxyz/pkg/renderer"
 	"io"
 	"net/http"
+
+	"github.com/abcxyz/pkg/renderer"
 )
 
 // DecodeRequest provides a common implementation of JSON unmarshaling with
@@ -39,7 +40,7 @@ func DecodeRequest[T any](ctx context.Context, w http.ResponseWriter, r *http.Re
 	if exp := "application/json"; len(t) < 16 || t[:16] != exp {
 		err = fmt.Errorf("invalid content type: content-type %q is not %q", t, exp)
 		h.RenderJSON(w, http.StatusUnsupportedMediaType, err)
-		return
+		return nil, err
 	}
 
 	defer r.Body.Close()
@@ -54,34 +55,34 @@ func DecodeRequest[T any](ctx context.Context, w http.ResponseWriter, r *http.Re
 		case errors.As(err, &syntaxErr):
 			err = fmt.Errorf("malformed json at position %d", syntaxErr.Offset)
 			h.RenderJSON(w, http.StatusBadRequest, err)
-			return
+			return nil, err
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			err = fmt.Errorf("malformed json")
 			h.RenderJSON(w, http.StatusBadRequest, err)
-			return
+			return nil, err
 		case errors.As(err, &unmarshalError):
 			err = fmt.Errorf("invalid value for %q at position %d (expected %s, got %s)",
 				unmarshalError.Field, unmarshalError.Offset, unmarshalError.Type, unmarshalError.Value)
 			h.RenderJSON(w, http.StatusBadRequest, err)
-			return
+			return nil, err
 		case errors.Is(err, io.EOF):
 			err = fmt.Errorf("body must not be empty")
 			h.RenderJSON(w, http.StatusBadRequest, err)
-			return
+			return nil, err
 		case err.Error() == "http: request body too large":
 			err = fmt.Errorf("request body too large")
 			h.RenderJSON(w, http.StatusRequestEntityTooLarge, err)
-			return
+			return nil, err
 		default:
 			err = fmt.Errorf("failed to decode request as json: %w", err)
 			h.RenderJSON(w, http.StatusBadRequest, err)
-			return
+			return nil, err
 		}
 	}
 	if d.More() {
 		err = fmt.Errorf("body contained more than one json object")
 		h.RenderJSON(w, http.StatusBadRequest, err)
-		return
+		return nil, err
 	}
-	return
+	return req, nil
 }
