41c248b7ea662f72b1d3ce83d47aa018b270ed98
Parker DeWilde <pdewilde@google.com>
2024-05-23 10:52:32 -0700

    Implement Metrics server. (#62)
    Very simple http server. This PR does not include code for fetching
    metrics or tests, which are implemented in subsequent PRs.

---
+++ cmd/main.go
@@ -0,0 +1,154 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main implements a simple HTTP/JSON REST example.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/abcxyz/abc-updater/pkg/metrics"
+	"github.com/abcxyz/abc-updater/pkg/server"
+	"github.com/abcxyz/pkg/logging"
+	"github.com/abcxyz/pkg/renderer"
+	"github.com/abcxyz/pkg/serving"
+)
+
+const defaultPort = "8080"
+
+var port = flag.String("port", defaultPort, "Specifies server port to listen on.")
+
+type AppMetrics struct {
+	AppID   string
+	Allowed map[string]interface{}
+}
+
+// MetricAllowed is a helper for looking up a particular metric for an app.
+func (m *AppMetrics) MetricAllowed(metric string) bool {
+	if m != nil && m.Allowed != nil {
+		_, ok := m.Allowed[metric]
+		return ok
+	}
+	return false
+}
+
+// GetAllowedMetrics returns a struct containing metrics for a given AppID.
+// An error is returned if that AppID is not defined in the backend for metrics.
+func GetAllowedMetrics(appID string) (*AppMetrics, error) {
+	// TODO: implement me
+	if appID != "implementLater" {
+		return nil, fmt.Errorf("no metric definition found for app %s", appID)
+	}
+	return &AppMetrics{Allowed: map[string]interface{}{"todo-implement": struct{}{}}}, nil
+}
+
+func handleMetric(h *renderer.Renderer) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := logging.FromContext(r.Context())
+		metricLogger := logger.WithGroup("metric")
+		logger.InfoContext(r.Context(), "handling request")
+
+		metrics, err := server.DecodeRequest[metrics.SendMetricRequest](r.Context(), w, r, h)
+		if err != nil {
+			// Error response already handled by pkg.DecodeRequest.
+			return
+		}
+
+		allowedMetrics, err := GetAllowedMetrics(metrics.AppID)
+		if err != nil {
+			h.RenderJSON(w, http.StatusNotFound, err)
+			logger.WarnContext(r.Context(), "received metric request for unknown app")
+			return
+		}
+
+		// Currently we only expose an API for a single metric on the client,
+		// but I suspect multiple metrics will be added later on, and effort is
+		// about the same to support both.
+		for name, count := range metrics.Metrics {
+			if allowedMetrics.MetricAllowed(name) {
+				// TODO: does this leak sensitive information? Is default logger preferred.
+				metricLogger.InfoContext(r.Context(), "metric received",
+					"app_id", metrics.AppID,
+					"app_version", metrics.AppVersion,
+					"install_id", metrics.InstallID,
+					"name", name,
+					"count", count)
+			} else {
+				// TODO: do we want to return a warning to client or fail silently?
+				logger.WarnContext(r.Context(), "received unknown metric for app", "app_id", metrics.AppID)
+			}
+		}
+
+		// Client does not currently read body, future changes are acceptable.
+		h.RenderJSON(w, http.StatusAccepted, map[string]string{"message": "ok"})
+	})
+}
+
+// realMain creates an example backend HTTP server.
+// This server supports graceful stopping and cancellation.
+func realMain(ctx context.Context) error {
+	logger := logging.FromContext(ctx)
+	// Make a new renderer for rendering json.
+	// Don't provide filesystem as we don't have templates to render.
+	h, err := renderer.New(ctx, nil,
+		renderer.WithOnError(func(err error) {
+			logger.ErrorContext(ctx, "failed to render", "error", err)
+		}))
+	if err != nil {
+		return fmt.Errorf("failed to create renderer for main server: %w", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("POST /sendMetrics", handleMetric(h))
+
+	httpServer := &http.Server{
+		Addr:              *port,
+		Handler:           mux,
+		ReadHeaderTimeout: 2 * time.Second,
+	}
+
+	logger.InfoContext(ctx, "starting server", "port", *port)
+	server, err := serving.New(*port)
+	if err != nil {
+		return fmt.Errorf("error creating server: %w", err)
+	}
+
+	// This will block until the provided context is cancelled.
+	if err := server.StartHTTP(ctx, httpServer); err != nil {
+		return fmt.Errorf("error starting server: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	// creates a context that exits on interrupt signal.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+	logger := logging.FromContext(ctx)
+
+	flag.Parse()
+	if err := realMain(logging.WithLogger(ctx, logger)); err != nil {
+		done()
+		logger.ErrorContext(ctx, err.Error())
+		os.Exit(1)
+	}
+	logger.InfoContext(ctx, "completed")
+}
---
+++ pkg/server/srvutil.go
@@ -0,0 +1,88 @@
+// Copyright 2024 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/abcxyz/pkg/renderer"
+)
+
+// DecodeRequest provides a common implementation of JSON unmarshaling with
+// well-defined error handling.
+//
+// Errors will be written to the provided response writer, with an error returned to the caller to alert them
+// no further processing should happen on the request.
+//
+// It automatically closes the request body to prevent leaking.
+// TODO: move this to abcxyz/pkg.
+func DecodeRequest[T any](ctx context.Context, w http.ResponseWriter, r *http.Request, h *renderer.Renderer) (*T, error) {
+	req := new(T)
+
+	t := r.Header.Get("content-type")
+	if exp := "application/json"; len(t) < 16 || t[:16] != exp {
+		err := fmt.Errorf("invalid content type: content-type %q is not %q", t, exp)
+		h.RenderJSON(w, http.StatusUnsupportedMediaType, err)
+		return nil, err
+	}
+
+	defer r.Body.Close()
+	body := http.MaxBytesReader(w, r.Body, 2<<20) // 2MiB
+
+	d := json.NewDecoder(body)
+
+	if err := d.Decode(&req); err != nil {
+		var syntaxErr *json.SyntaxError
+		var unmarshalError *json.UnmarshalTypeError
+		switch {
+		case errors.As(err, &syntaxErr):
+			err = fmt.Errorf("malformed json at position %d", syntaxErr.Offset)
+			h.RenderJSON(w, http.StatusBadRequest, err)
+			return nil, err
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			err = fmt.Errorf("malformed json")
+			h.RenderJSON(w, http.StatusBadRequest, err)
+			return nil, err
+		case errors.As(err, &unmarshalError):
+			err = fmt.Errorf("invalid value for %q at position %d (expected %s, got %s)",
+				unmarshalError.Field, unmarshalError.Offset, unmarshalError.Type, unmarshalError.Value)
+			h.RenderJSON(w, http.StatusBadRequest, err)
+			return nil, err
+		case errors.Is(err, io.EOF):
+			err = fmt.Errorf("body must not be empty")
+			h.RenderJSON(w, http.StatusBadRequest, err)
+			return nil, err
+		case err.Error() == "http: request body too large":
+			err = fmt.Errorf("request body too large")
+			h.RenderJSON(w, http.StatusRequestEntityTooLarge, err)
+			return nil, err
+		default:
+			err = fmt.Errorf("failed to decode request as json: %w", err)
+			h.RenderJSON(w, http.StatusBadRequest, err)
+			return nil, err
+		}
+	}
+	if d.More() {
+		err := fmt.Errorf("body contained more than one json object")
+		h.RenderJSON(w, http.StatusBadRequest, err)
+		return nil, err
+	}
+	return req, nil
+}
