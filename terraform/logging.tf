6c78f9ca1773a3c8f274b02a55c62d7149a604fd
Parker DeWilde <pdewilde@google.com>
2024-06-27 17:06:02 -0700

    Route metrics logs to dedicated cloud logging bucket (#78)
    This bucket will only have logs containing the raw metrics, and is
    enabled for analytics for easy querying.

---
+++ terraform/logging.tf
@@ -0,0 +1,36 @@
+# Copyright 2024 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_logging_project_bucket_config" "metrics" {
+  project = var.project_id
+
+  location         = var.compute_region
+  retention_days   = var.metrics_log_bucket_retention_days
+  bucket_id        = var.metrics_log_bucket_name
+  enable_analytics = true
+}
+
+resource "google_logging_project_sink" "metrics" {
+  project = var.project_id
+
+  name                   = "abc-updater-metrics-sink"
+  destination            = "logging.googleapis.com/${google_logging_project_bucket_config.metrics.name}"
+  unique_writer_identity = true
+  filter = <<EOF
+  resource.type="cloud_run_revision" AND
+  resource.labels.service_name="${var.metrics_service_name}" AND
+  jsonPayload.message="metric received"
+  EOF
+
+}
