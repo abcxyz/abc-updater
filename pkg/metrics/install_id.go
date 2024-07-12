29fcfee5ecc3404012e4e9b8c05014afac2c94e7
pdewilde <pdewilde@google.com>
2024-07-12 15:54:45 -0700

    Fix comment documenting install ID

--- pkg/metrics/install_id.go
+++ pkg/metrics/install_id.go
@@ -25,7 +25,7 @@ import (
 
 // InstallIDData defines the json file that defines installation id.
 type InstallIDData struct {
-	// InstallID. Expected to be a hex 8-4-4-4-12 formatted v4 UUID.
+	// InstallID. 8 random bytes base64-encoded.
 	InstallID string `json:"installId"`
 }
 
